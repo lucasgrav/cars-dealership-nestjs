e7643f41774cdd2f4443fd8ba4c77f8e93375e6f
Lucas Correa <correalucas1905@gmail.com>
2023-11-17 14:30:01 -0300

    feat(cars): add crud

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,13 @@
-import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Param,
+  ParseIntPipe,
+  Post,
+  Body,
+  Patch,
+  Delete,
+} from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -12,5 +21,23 @@ export class CarsController {
   @Get(':id')
   getCardById(@Param('id', ParseIntPipe) id: number) {
     return this.carsService.findOneById(id);
+  }
+
+  @Post()
+  createCar(@Body() body: any) {
+    return body;
+  }
+
+  @Patch(':id')
+  updateCar(@Param('id', ParseIntPipe) id: number, @Body() body: any) {
+    return body;
+  }
+
+  @Delete(':id')
+  deleteCar(@Param('id', ParseIntPipe) id: number) {
+    return {
+      method: 'Delete',
+      id,
+    };
   }
 }
