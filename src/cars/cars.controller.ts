c39823759e865b3aa45e31b7daf1f7dd2aee4dac
Lucas Correa <correalucas1905@gmail.com>
2023-11-17 13:45:26 -0300

    feat(cars): add pipes

--- src/cars/cars.controller.ts
+++ src/cars/cars.controller.ts
@@ -1,4 +1,4 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Controller, Get, Param, ParseIntPipe } from '@nestjs/common';
 import { CarsService } from './cars.service';
 
 @Controller('cars')
@@ -10,7 +10,7 @@ export class CarsController {
   }
 
   @Get(':id')
-  getCardById(@Param('id') id: string) {
-    return this.carsService.findOneById(+id);
+  getCardById(@Param('id', ParseIntPipe) id: number) {
+    return this.carsService.findOneById(id);
   }
 }
